b39d89130267a7628169e9ded8e109c55036e07e
Marcus Frenje <48588529+Frenje123@users.noreply.github.com>
2023-01-12 20:55:10 +0100

    Add primary keys (#10)

---
+++ TelstarLogistics/TelstarLogistics/Controllers/Api/BookingController.cs
@@ -0,0 +1,19 @@
+﻿using TelstarLogistics.Models;
+using TelstarLogistics.Models.ApiModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TelstarLogistics.Controllers.Api
+{
+    [Route("booking-api")]
+    [ApiController]
+    public class BookingController : Controller
+    {
+        [HttpGet]
+        [Route("GetEmployeeOfMonth")]
+        public async Task<ActionResult> GetEmployeeOfMonth()
+        {
+            //var DbContext = new TelstarLogisticsContext();
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
---
+++ TelstarLogistics/TelstarLogistics/Migrations/20230112195003_AddPrimaryKey.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelstarLogistics.Migrations
+{
+    public partial class AddPrimaryKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Route",
+                table: "Route",
+                column: "RouteId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_City",
+                table: "City",
+                column: "CityId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Booking",
+                table: "Booking",
+                column: "BookingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Route",
+                table: "Route");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_City",
+                table: "City");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Booking",
+                table: "Booking");
+        }
+    }
+}
