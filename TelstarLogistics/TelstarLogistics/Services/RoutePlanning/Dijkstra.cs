a6819e4adea7d0a75735619fc9ca3c4af6ebfee4
PerKristianNet <perkrs@netcompany.com>
2023-01-12 23:36:13 +0100

    wp4:data integration preparation

--- TelstarLogistics/TelstarLogistics/Services/RoutePlanning/Dijkstra.cs
+++ TelstarLogistics/TelstarLogistics/Services/RoutePlanning/Dijkstra.cs
@@ -7,23 +7,23 @@ namespace TelstarLogistics.Services.RoutePlanning;
 public class Dijkstra
 {
     private Dictionary<int,int> cityIdToGraphIdx;
-    private int[,] graph;
+    private (int, string)[,] graph;
     private List<City> cities;
 
 
-    //public string TEST(string from, string to)
-    //{
-    //    //var search = new Dijkstra();
-    //    //var distance = search.GetRoute(from.ToLower(), to.ToLower(), out var path);
+    public string TEST(string from, string to)
+    {
+        var search = new Dijkstra();
+        var distance = search.GetRoute(from.ToLower(), to.ToLower(),false, out var path);
 
-    //    //string output = ($"distance:{distance} \n");
-    //    //foreach (var city in path)
-    //    //{
-    //    //    output += ($"{city} -> ");
-    //    //}
+        string output = ($"distance:{distance} \n");
+        foreach (var city in path)
+        {
+            output += ($"{city} -> ");
+        }
 
-    //    //return output;
-    //}
+        return output;
+    }
 
 
     public int GetRoute(string? from, string? to, bool reccommended, out List<string> path)
@@ -33,27 +33,118 @@ public class Dijkstra
         var toCity = cities.FirstOrDefault(city => city.Name == to);
         if (cityIdToGraphIdx.TryGetValue(fromCity.CityId,out int fromIdx) && cityIdToGraphIdx.TryGetValue(toCity.CityId, out int toIdx))
         {
-            var distances = ComputePaths(graph, fromIdx, out var parents);
-            path = GetPath(toIdx, parents).Select(id => cities[id].Name).ToList();
-            return distances[toIdx];
+            var distances = ComputePaths(graph, fromIdx, out var parents, out var transportTypes, out int[] carDistance);
+            path = GetPath(toIdx, parents, transportTypes).Select(pathnode => $"{pathnode.Item2},{cities[pathnode.Item1].Name}").ToList();
+            return carDistance[toIdx];
         }
 
         path = new List<string>() { "no" };
         return -1;
     }
 
-
-
-    private int[,] CreateGraph(bool recommended)
+    private (int, string)[,] CreateGraph(bool recommended)
     {
         var context = new TelstarLogisticsContext();
 
         cities = context.Cities.ToList();
+        //cities = new List<City>
+        //{
+        //    new City
+        //    {
+        //        Name = "c1",
+        //        CityId = 1
+        //    },
+        //    new City
+        //    {
+        //        Name = "c2",
+        //        CityId = 2
+        //    },
+        //    new City
+        //    {
+        //        Name = "c3",
+        //        CityId = 3
+        //    },
+        //    new City
+        //    {
+        //        Name = "c4",
+        //        CityId = 4
+        //    },
+        //    new City
+        //    {
+        //        Name = "c0",
+        //        CityId = 0
+        //    },
+        //    new City
+        //    {
+        //        Name = "c5",
+        //        CityId = 5
+        //    },
+        //};
         cityIdToGraphIdx = cities.ToDictionary(city => city.CityId, city => cities.IndexOf(city));
 
-        var graph = new int[cities.Count, cities.Count];
+        var graph = new (int, string)[cities.Count, cities.Count];
 
         var routes = recommended ? context.Routes.Where(route => route.TransportType == "car").ToList() : context.Routes.ToList();
+        //var routes = new List<Route>
+        //{
+        //    new Route
+        //    {
+        //        City1Id = 1,
+        //        City2Id = 2,
+        //        Distance = 10,
+        //        TransportType = "car"
+        //    },
+        //    new Route
+        //    {
+        //        City1Id = 2,
+        //        City2Id = 3,
+        //        Distance = 10,
+        //        TransportType = "car"
+        //    },
+        //    new Route
+        //    {
+        //        City1Id = 3,
+        //        City2Id = 4,
+        //        Distance = 10,
+        //        TransportType = "car"
+        //    },
+        //    new Route
+        //    {
+        //        City1Id = 4,
+        //        City2Id = 1,
+        //        Distance = 10,
+        //        TransportType = "car"
+        //    },
+        //    new Route
+        //    {
+        //        City1Id = 1,
+        //        City2Id = 3,
+        //        Distance = 1,
+        //        TransportType = "plane"
+        //    },
+        //    new Route
+        //    {
+        //        City1Id = 0,
+        //        City2Id = 1,
+        //        Distance = 5,
+        //        TransportType = "car"
+        //    },
+        //    new Route
+        //    {
+        //        City1Id = 3,
+        //        City2Id = 5,
+        //        Distance = 5,
+        //        TransportType = "car"
+        //    },
+        //    new Route
+        //    {
+        //        City1Id = 0,
+        //        City2Id = 5,
+        //        Distance = 20,
+        //        TransportType = "boat"
+        //    },
+
+        //};
 
         for (int i = 0; i < routes.Count; i++)
         {
@@ -62,18 +153,25 @@ public class Dijkstra
             var city1 = cityIdToGraphIdx[route.City1Id];
             var city2 = cityIdToGraphIdx[route.City2Id];
 
-            graph[city1, city2] = route.Distance;
-            graph[city2, city2] = route.Distance;
+            if (graph[city1, city2].Item2 == "car")
+            {
+                continue;
+            }
+
+            graph[city1, city2] = (route.Distance, route.TransportType);
+            graph[city2, city1] = (route.Distance, route.TransportType);
         }
 
         return graph;
     }
 
-    public static int[] ComputePaths(int[,] graph, int sourceNode, out int[] parent)
+    public static int[] ComputePaths((int, string)[,] graph, int sourceNode, out int[] parent, out string[] TransportType, out int[] carDistance)
     {
         int n = graph.GetLength(0);
         int[] distance = new int[n];
         bool[] visited = new bool[n];
+        carDistance = new int[n];
+        TransportType = new string[n];
         parent = new int[n];
 
         for (int i = 0; i < n; i++)
@@ -90,10 +188,13 @@ public class Dijkstra
 
             for (int j = 0; j < n; j++)
             {
-                if (!visited[j] && graph[minDistanceNode, j] != 0 && distance[minDistanceNode] != int.MaxValue && distance[minDistanceNode] + graph[minDistanceNode, j] < distance[j])
+                if (!visited[j] && graph[minDistanceNode, j].Item1 != 0 && distance[minDistanceNode] != int.MaxValue && distance[minDistanceNode] + graph[minDistanceNode, j].Item1 < distance[j])
                 {
-                    distance[j] = distance[minDistanceNode] + graph[minDistanceNode, j];
+                    distance[j] = distance[minDistanceNode] + graph[minDistanceNode, j].Item1;
+                    int cardist = graph[minDistanceNode, j].Item2 == "car" ? graph[minDistanceNode, j].Item1 : 0;
+                    carDistance[j] = carDistance[minDistanceNode] + cardist;
                     parent[j] = minDistanceNode;
+                    TransportType[j] = graph[minDistanceNode, j].Item2;
                 }
             }
         }
@@ -116,28 +217,28 @@ public class Dijkstra
         return minIndex;
     }
 
-    public static List<int> GetPath(int endVertex, int[] parents)
+    public static List<(int, string)> GetPath(int endVertex, int[] parents, string[] TransportTypes)
     {
-        List<int> path = RetracePath(endVertex, parents);
+        List<(int, string)> path = RetracePath(endVertex, parents, TransportTypes);
         path.Reverse();
         return path;
     }
 
-    public static List<int> RetracePath(int endVertex, int[] parents)
+    public static List<(int, string)> RetracePath(int endVertex, int[] parents, string[] TransportTypes)
     {
         if (endVertex < 0 || endVertex > parents.Length) throw new ArgumentOutOfRangeException(nameof(endVertex));
 
-        var path = new List<int>();
+        var path = new List<(int, string)>();
 
         int count = 0; //loop failsafe
         int idx = endVertex; //move from end index towards start
         while (parents[idx] >= 0 && count < parents.Length)
         {
-            path.Add(idx);
+            path.Add((idx, TransportTypes[idx]));
             idx = parents[idx];
             count++;
         }
-        path.Add(idx);
+        path.Add((idx, ""));
 
         return path;
     }
