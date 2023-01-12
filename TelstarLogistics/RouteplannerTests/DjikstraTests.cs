44b741ffe5e846f95ab3c9d4521aec052dc6d662
PerKristianNet <perkrs@netcompany.com>
2023-01-12 14:42:17 +0100

    wp4:Routplanning
    Dijkstra added with retracing steps to target

---
+++ TelstarLogistics/RouteplannerTests/DjikstraTests.cs
@@ -0,0 +1,57 @@
+
+
+using Humanizer;
+
+namespace RouteplannerTests
+{
+    [TestClass]
+    public class DjikstraTests
+    {
+        [TestMethod]
+        public void TestFindShortestPath()
+        {
+            //0 connect to 2
+            //1 connect to 2
+            //2 connect to 0 and 1
+            int[,] graph = new int[,]
+            {
+                { 0, 0, 2 }, //0 connections
+                { 0, 0, 2 }, //1 ----||----
+                { 2, 2, 0 }, //2 ----||----
+            };
+            int sourceNode = 0;
+            int destinationNode = 1;
+
+
+
+            int[] distances = TelstarLogistics.Services.RoutePlanning.Dijkstra.ComputePaths(graph, sourceNode, out var parents);
+
+            Assert.AreEqual(4,distances[destinationNode]);
+        }
+
+        [TestMethod]
+        public void TestRetracePath()
+        {
+            //0 connect to 2
+            //1 connect to 2
+            //2 connect to 0 and 1
+            int[,] graph = new int[,]
+            {
+                { 0, 0, 2 }, //0 connections
+                { 0, 0, 2 }, //1 ----||----
+                { 2, 2, 0 }, //2 ----||----
+            };
+            int sourceNode = 0;
+            int destinationNode = 1;
+
+
+
+            int[] distances = TelstarLogistics.Services.RoutePlanning.Dijkstra.ComputePaths(graph, sourceNode, out var parents);
+            var path = TelstarLogistics.Services.RoutePlanning.Dijkstra.GetPath(destinationNode, parents);
+            Assert.IsTrue(path[0] == 0 && path[1] == 2 && path[2] == 1);
+        }
+    }
+
+
+
+}
---
+++ TelstarLogistics/TelstarLogistics/Services/RoutePlanning/DijkstraGraph.cs
@@ -0,0 +1,66 @@
+﻿namespace TelstarLogistics.Services.RoutePlanning;
+
+public class DijkstraGraph
+{
+
+    private Dictionary<int, int> GraphIdxToCityId;
+    private int[,] graph;
+
+    private int[,] CreateGraph()
+    {
+        //get from -> to
+        //get all cities
+        //get connections to all other cities
+        int cityCount = 3;
+        //map city to int
+
+        var graph = new int[cityCount,cityCount];
+        //iterate all connections
+
+        return graph;
+
+    }
+
+    
+
+    private static string printSolution(int startVertex,
+        int[] distances,
+        int[] parents)
+    {
+        string s = "";
+        int nVertices = distances.Length;
+        s += ("Vertex\t Distance\tPath");
+
+        for (int vertexIndex = 0;
+             vertexIndex < nVertices;
+             vertexIndex++)
+        {
+            if (vertexIndex != startVertex)
+            {
+                s += ("\n" + startVertex + " -> ");
+                s += (vertexIndex + " \t\t ");
+                s += (distances[vertexIndex] + "\t\t");
+                printPath(vertexIndex, parents, ref s);
+
+            }
+        }
+
+        return s;
+    }
+
+    private static void printPath(int currentVertex,
+        int[] parents, ref string s)
+    {
+
+        // Base case : Source node has
+        // been processed
+        if (currentVertex == -1)
+        {
+            return;
+        }
+
+        printPath(parents[currentVertex], parents, ref s);
+        s += (currentVertex + " ");
+
+    }
+}
