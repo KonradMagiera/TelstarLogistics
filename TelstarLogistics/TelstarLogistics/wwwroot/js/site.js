c46b8e67faa8313d49473be877bd1a5ba56aa63a
Marcin Guzy <guzy@netcompany.com>
2023-01-12 18:03:16 +0100

    wp3 ui init

--- TelstarLogistics/TelstarLogistics/wwwroot/js/site.js
+++ TelstarLogistics/TelstarLogistics/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+const modal = docuement.querySelector("#modal")
+const openModal = docuement.querySelector(".open-button")
+const closeModal = docuement.querySelector(".close-button")
+
+openModal.addEventListener('click', () => {
+    modal.showModal()
+})
+
+closeModal.addEventListener('click', () => {
+    modal.showModal()
+})
+
+
