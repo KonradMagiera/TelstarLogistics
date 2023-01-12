2a6c28f83f1e5f2f587d032fec72f878857561ec
Cristian Florin Pandele <cfp@netcompany.com>
2023-01-12 17:24:25 +0100

    Updated get route request in the internal api

--- TelstarLogistics/TelstarLogistics/Models/ApiModel/GetRoutesRequest.cs
+++ TelstarLogistics/TelstarLogistics/Models/ApiModel/GetRoutesRequest.cs
@@ -4,8 +4,13 @@ namespace TelstarLogistics.Models.ApiModel
 {
     public class GetRoutesRequest
     {
+        public string from { get; set; }
         public string to { get; set; }
         public string type { get; set; }
-
+        public float weight { get; set; }
+        public float height { get; set; }
+        public float width { get; set; }
+        public float depth { get; set; }
+        public bool recommended { get; set; }
     }
 }
