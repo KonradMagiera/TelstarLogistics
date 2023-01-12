54f77ebed68a5b7b439c420036d03b015da810fb
Marcus Frenje <48588529+Frenje123@users.noreply.github.com>
2023-01-13 00:45:16 +0100

    Fix migration (#18)
    * Fix migration errors
    
    * remove code from migration

--- TelstarLogistics/TelstarLogistics/Migrations/20230112233126_FixErrors.cs
+++ TelstarLogistics/TelstarLogistics/Migrations/20230112233126_FixErrors.cs
@@ -8,40 +8,10 @@ namespace TelstarLogistics.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "Cost",
-                table: "Route");
-
-            migrationBuilder.RenameColumn(
-                name: "StartCityId",
-                table: "Route",
-                newName: "Distance");
-
-            migrationBuilder.AddColumn<int>(
-                name: "City1Id",
-                table: "Route",
-                type: "int",
-                nullable: false,
-                defaultValue: 0);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "City1Id",
-                table: "Route");
-
-            migrationBuilder.RenameColumn(
-                name: "Distance",
-                table: "Route",
-                newName: "StartCityId");
-
-            migrationBuilder.AddColumn<decimal>(
-                name: "Cost",
-                table: "Route",
-                type: "decimal(18,0)",
-                nullable: false,
-                defaultValue: 0m);
         }
     }
 }
