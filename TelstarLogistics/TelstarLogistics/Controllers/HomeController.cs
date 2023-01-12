dae0d6b7faf7ad116842264cefed502633fcfddb
Marcin Guzy <guzy@netcompany.com>
2023-01-13 00:26:38 +0100

    wp3 integrations part1

--- TelstarLogistics/TelstarLogistics/Controllers/HomeController.cs
+++ TelstarLogistics/TelstarLogistics/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Diagnostics;
 using TelstarLogistics.Data;
 using TelstarLogistics.Models;
@@ -14,8 +16,42 @@ namespace TelstarLogistics.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            User _user = new User();
+            return View(_user);
+        }
+
+        [HttpPost]
+        public IActionResult Index(User _user)
+        {
+            TelstarLogisticsContext _telstarLogistics = new TelstarLogisticsContext();
+            var test = _user;
+            var muser = _user.UserName;
+            var searchedUser = _telstarLogistics.Users.FirstOrDefault(m =>
+              EF.Functions.Like(m.UserName, _user.UserName) && EF.Functions.Like(m.Password, _user.Password));
+            if(searchedUser == null)
+            {
+                ViewBag.LoginStatus = 0;
+            } else
+            {
+                ViewBag.LoginStatus = 1;
+                ViewBag.User = searchedUser;
+                TempData["UserRole"] = searchedUser.Role;
+                TempData["UserName"] = searchedUser.UserName;
+
+                return RedirectToAction("MainPage");
+            }
+            return View(_user);
+        }
+
+
+        public IActionResult MainPage()
+        {
+            ViewBag.UserRole = TempData["UserRole"].ToString();
+            ViewBag.UserName = TempData["UserName"].ToString();
+           
             return View();
         }
 
