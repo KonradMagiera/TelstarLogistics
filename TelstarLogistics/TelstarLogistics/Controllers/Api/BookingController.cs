a6ea69501b5d5735853043f7f3d686d48fc82a92
Marcus Frenje <48588529+Frenje123@users.noreply.github.com>
2023-01-12 21:23:58 +0100

    Add BookingRevenue field to Bookings (#11)

--- TelstarLogistics/TelstarLogistics/Controllers/Api/BookingController.cs
+++ TelstarLogistics/TelstarLogistics/Controllers/Api/BookingController.cs
@@ -1,6 +1,8 @@
 ﻿using TelstarLogistics.Models;
 using TelstarLogistics.Models.ApiModel;
 using Microsoft.AspNetCore.Mvc;
+using TelstarLogistics.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace TelstarLogistics.Controllers.Api
 {
@@ -12,7 +14,11 @@ namespace TelstarLogistics.Controllers.Api
         [Route("GetEmployeeOfMonth")]
         public async Task<ActionResult> GetEmployeeOfMonth()
         {
-            //var DbContext = new TelstarLogisticsContext();
+            var Bookings = new TelstarLogisticsContext().Bookings;
+            foreach (var booking in Bookings)
+            {
+                //booking.pri
+            }
             return Ok(new string[] { "value1", "value2" });
         }
     }
