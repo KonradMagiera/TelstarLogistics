8c55e60b74d65da41b09b48708b6c1650b3334e7
Cristian Florin Pandele <cfp@netcompany.com>
2023-01-13 11:26:50 +0100

    bug8 refactor getroutes method

---
+++ TelstarLogistics/TelstarLogistics/Models/ApiModel/IntegrationRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace TelstarLogistics.Models.ApiModel
+{
+    public class IntegrationRequest
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Type { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public string Currency { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public bool Recommended { get; set; }
+
+    }
+}
