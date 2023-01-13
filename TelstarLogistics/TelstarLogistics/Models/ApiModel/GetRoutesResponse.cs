9ff8fcfde6b4ee2b52db45f9c329cdec940f8278
Cristian Florin Pandele <cfp@netcompany.com>
2023-01-13 02:14:10 +0100

    WP7: added dijkstra to API

---
+++ TelstarLogistics/TelstarLogistics/Models/ApiModel/GetRoutesResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TelstarLogistics.Models.ApiModel
+{
+    public class GetRoutesResponse
+    {
+        public string RouteType { get; set; }
+        public DateTime DeliveryTime { get; set; }
+        public float Price { get; set; }
+        public List<string> Path { get; set; }
+    }
+}
