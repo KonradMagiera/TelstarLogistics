d49fb2d2cd97e213ad3c7da94793b3f309a5a0d6
Marcin Guzy <guzy@netcompany.com>
2023-01-13 03:45:56 +0100

    wp3 get routes

--- TelstarLogistics/TelstarLogistics/Controllers/HomeController.cs
+++ TelstarLogistics/TelstarLogistics/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using TelstarLogistics.Data;
 using TelstarLogistics.Models;
 using TelstarLogistics.Models.ApiModel;
 using TelstarLogistics.Services.RoutePlanning;
+using static Humanizer.In;
 
 namespace TelstarLogistics.Controllers
 {
@@ -53,6 +54,7 @@ namespace TelstarLogistics.Controllers
         }
 
 
+        [HttpGet]
         public IActionResult MainPage()
         {
             BookingController bookingController = new BookingController();
@@ -76,35 +78,39 @@ namespace TelstarLogistics.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> submit([FromBody] GetRoutesRequest request)
-        {
+        public async Task<ActionResult> MainPage(String source, String destination, String parcelType, int  weight, int height, int length, int width, DateTime handover, Boolean recommended)
+        {        
+
+            Boolean recomend = recommended != null && recommended;
+           
+
             double priceMultiplier = 1.00;
             double recommendPriceAddition = 0;
-            if (request.recommended == true)
+            if (recommended == true)
             {
                 recommendPriceAddition = 10;
             }
-            else if (request.type == "liveAnimals")
+            else if (parcelType == "liveAnimals")
             {
                 priceMultiplier = 1.50;
             }
-            else if (request.type == "cautionsParcels")
+            else if (parcelType == "cautionsParcels")
             {
                 priceMultiplier = 1.75;
             }
-            else if (request.type == "refrigertedGoods")
+            else if (parcelType == "refrigertedGoods")
             {
                 priceMultiplier = 1.10;
             }
             Dijkstra dijkstra = new Dijkstra();
 
-            var travelDistance = dijkstra.GetRoute(request.from, request.to, false, true, out var path);
+            var travelDistance = dijkstra.GetRoute(source, destination, false, true, out var path);
             List<GetRoutesResponse> routes = new List<GetRoutesResponse>();
 
             routes.Add(new GetRoutesResponse { RouteType = "fastest", DeliveryTime = new DateTime().AddHours(travelDistance * 4), Price = travelDistance * 3, Path = path });
 
 
-            var travelDistance2 = dijkstra.GetRoute(request.from, request.to, true, false, out var path2);
+            var travelDistance2 = dijkstra.GetRoute(source, destination, true, false, out var path2);
 
             routes.Add(new GetRoutesResponse { RouteType = "best", DeliveryTime = new DateTime().AddHours(travelDistance2 * 4), Price = travelDistance2 * 3, Path = path2 });
 
@@ -118,7 +124,21 @@ namespace TelstarLogistics.Controllers
 
             // response: Provides list of routes, one for each of types (best, Cheapest, Shortest) 
 
-            return Ok(routes);
+            BookingController bookingController = new BookingController();
+            List<City> cityList = dbContext.Cities.ToList();
+            if (TempData["UserRole"] != null)
+            {
+                ViewBag.UserName = TempData["UserName"].ToString();
+            }
+            if (TempData["UserName"] != null)
+            {
+                ViewBag.UserName = TempData["UserName"].ToString();
+            }
+            ViewBag.Cities = cityList;
+            ViewBag.EOTM = bookingController.GetEmployeeOfMonth().GetAwaiter().GetResult().ToString();
+            ViewBag.Results = routes;
+
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
